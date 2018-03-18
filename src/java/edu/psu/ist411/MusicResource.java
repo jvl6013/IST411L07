0f710f4150f4b0b88a717e1829a17648061f9d05
Owen Lee <jvl6013@psu.edu>
2018-03-18 15:58:18 -0400

    Initial commit

---
+++ src/java/edu/psu/ist411/MusicResource.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.psu.ist411;
+
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.Produces;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PUT;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * REST Web Service
+ *
+ * @author myphs
+ */
+@Path("music")
+public class MusicResource {
+
+    @Context
+    private UriInfo context;
+
+    /**
+     * Creates a new instance of MusicResource
+     */
+    public MusicResource() {
+    }
+
+    /**
+     * Retrieves representation of an instance of edu.psu.ist411.MusicResource
+     * @return an instance of java.lang.String
+     */
+    @GET
+    @Produces(MediaType.TEXT_HTML)
+    public String getHtml() {
+        //TODO return proper representation object
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * PUT method for updating or creating an instance of MusicResource
+     * @param content representation for the resource
+     */
+    @PUT
+    @Consumes(MediaType.TEXT_HTML)
+    public void putHtml(String content) {
+    }
+}
